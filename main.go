d220b0294e4a213f95cb539404f82227818968f8
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-07-10 05:44:06 -0300

    Working in initial stages of making persistent local storage. Its buggy, and currently the txt db file somehow is getting corrupted by something..but it works! So thats a win, the code is messy right now, but in a couple new refactorings and fixing the saving to db/reading/writing from db bugs, the feature should be done

--- main.go
+++ main.go
@@ -3,27 +3,50 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func main() {
+
+
+
+
+
+func InnerLogic(db *os.File) {
+	scanner := bufio.NewScanner(db)
+	writer := bufio.NewWriter(db)
 	var toDoList []string
-	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan(){
+		task := scanner.Text()
+		toDoList = append(toDoList,task )
+	}
+	
+	scanner = bufio.NewScanner(os.Stdin)
+
+
+
 	fmt.Println("Welcome To Your Task Management System!")
 	fmt.Println("Please input 'Tasks' to see any current Tasks")
 	fmt.Println("Please input 'Add' to add a new Task")
 	fmt.Println("Please input 'Remove' to remove a completed Task")
 	fmt.Println("Please input 'Exit()' to exit the program")
+
+
+
 	isOn := true
+
 	for isOn {
 		fmt.Println("\nInput New Command:")
 		scanner.Scan()
+
 		command := strings.ToLower(scanner.Text())
 		fmt.Printf("You said: '%v'\n", command)
 
+
 		switch command {
+
 		case "tasks":
 			if len(toDoList) == 0 {
 				fmt.Println("No Tasks Pending!")
@@ -33,11 +56,27 @@ func main() {
 			for i, task := range toDoList {
 				fmt.Printf("  %v. %v\n", i+1, task)
 			}
+
+
 		case "add":
 			fmt.Println("What is your new task?")
 			scanner.Scan()
 			toDoList = append(toDoList, scanner.Text())
+			os.Create(db.Name())
+			for _, task := range toDoList{
+				_,err := writer.WriteString(task+"\n")
+				if err != nil {
+					log.Fatal("Error Writing: ",err)
+				}
+			}
+			err := writer.Flush()
+			if err != nil {
+					log.Fatal("Error flushing: ",err)
+				}
 			fmt.Println("Task Added!")
+
+
+
 		case "remove":
 			if len(toDoList) == 0 {
 				fmt.Println("No Tasks To Remove!")
@@ -56,14 +95,57 @@ func main() {
 				continue
 			}
 			toDoList = append(toDoList[:index], toDoList[index+1:]...)
+			os.Create(db.Name())
+
+			for _, task := range toDoList{
+				_,err := writer.WriteString(task+"\n")
+				if err != nil {
+					log.Fatal("Error Writing: ",err)
+				}
+			}
+			err = writer.Flush()
+			if err != nil {
+					log.Fatal("Error flushing: ",err)
+				}
 			fmt.Println("Task Eliminated!")
+
+
+
 		case "exit()":
 			fmt.Println("Goodbye...")
 			isOn = false
 			continue
+
+
 		default:
 			fmt.Println("Unknown Command")
 		}
 
+
 	}
+  }	
+
+
+
+
+func main() {
+	db_name :="db.txt"
+
+	if _, err := os.Stat(db_name); err != nil{
+		db, err := os.Create(db_name)
+		defer db.Close()
+		if err != nil{
+			log.Fatal("Error in ",err)
+		}
+		InnerLogic(db)
+	}
+
+	db, err := os.OpenFile(db_name,os.O_RDWR, 0644)
+	defer db.Close()
+	if err != nil{
+		log.Fatal("Error, Could Not OpenFile Line 108: ", err)
+	}
+	InnerLogic(db)
+
+
 }
