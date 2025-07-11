e80624c47941da7a364d8c59ee20f4e06ae42228
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-07-10 21:36:28 -0300

    Fixed DB corruption bug, turned out to be problem with using os create but still referencing to old db, probably was some weird memory issue, also organized the code a little but not by much, next will work on organizing it all into neat functions, and then work on implementing better cli support. After that we will look into web servers and http as well as json stuff we can do

--- main.go
+++ main.go
@@ -12,27 +12,52 @@ import (
 
 
 
-
-
-func InnerLogic(db *os.File) {
-	scanner := bufio.NewScanner(db)
-	writer := bufio.NewWriter(db)
-	var toDoList []string
-	for scanner.Scan(){
-		task := scanner.Text()
-		toDoList = append(toDoList,task )
-	}
-	
-	scanner = bufio.NewScanner(os.Stdin)
-
-
-
+func welcomeMessage(){
 	fmt.Println("Welcome To Your Task Management System!")
 	fmt.Println("Please input 'Tasks' to see any current Tasks")
 	fmt.Println("Please input 'Add' to add a new Task")
 	fmt.Println("Please input 'Remove' to remove a completed Task")
 	fmt.Println("Please input 'Exit()' to exit the program")
+}
 
+func getFromDB(db *os.File) []string{
+	DBscanner := bufio.NewScanner(db)
+	
+	var toDoList []string
+	for DBscanner.Scan(){
+		task := DBscanner.Text()
+		toDoList = append(toDoList,task )
+	}
+	return toDoList
+}
+
+func VerifyDB() *os.File{
+	db_name :="db.txt"
+	if _, err := os.Stat(db_name); err != nil{
+		db, err := os.Create(db_name)
+		
+		if err != nil{
+			log.Fatal("Error in ",err)
+		}
+		return db
+		
+	}
+
+	db, err := os.OpenFile(db_name,os.O_RDWR, 0644)
+	if err != nil{
+		log.Fatal("Error, Could Not OpenFile Line 108: ", err)
+	}
+	return db
+	
+
+	}
+
+func InnerLogic(db *os.File, toDoList []string) {
+	
+	scanner := bufio.NewScanner(os.Stdin)
+
+	welcomeMessage()
+	writer := bufio.NewWriter(db)
 
 
 	isOn := true
@@ -62,14 +87,19 @@ func InnerLogic(db *os.File) {
 			fmt.Println("What is your new task?")
 			scanner.Scan()
 			toDoList = append(toDoList, scanner.Text())
-			os.Create(db.Name())
+			db, err := os.Create(db.Name())
+			
+			if err != nil{
+				log.Fatal("Error, Data Deleted, Please Verify ", err)
+			}
+			writer = bufio.NewWriter(db)
 			for _, task := range toDoList{
 				_,err := writer.WriteString(task+"\n")
 				if err != nil {
 					log.Fatal("Error Writing: ",err)
 				}
 			}
-			err := writer.Flush()
+			err = writer.Flush()
 			if err != nil {
 					log.Fatal("Error flushing: ",err)
 				}
@@ -95,7 +125,11 @@ func InnerLogic(db *os.File) {
 				continue
 			}
 			toDoList = append(toDoList[:index], toDoList[index+1:]...)
-			os.Create(db.Name())
+			db, err = os.Create(db.Name())
+			writer = bufio.NewWriter(db)
+			if err != nil{
+				log.Fatal("Error In deleting Data ", err)
+			}
 
 			for _, task := range toDoList{
 				_,err := writer.WriteString(task+"\n")
@@ -129,23 +163,8 @@ func InnerLogic(db *os.File) {
 
 
 func main() {
-	db_name :="db.txt"
-
-	if _, err := os.Stat(db_name); err != nil{
-		db, err := os.Create(db_name)
-		defer db.Close()
-		if err != nil{
-			log.Fatal("Error in ",err)
-		}
-		InnerLogic(db)
-	}
-
-	db, err := os.OpenFile(db_name,os.O_RDWR, 0644)
+	db := VerifyDB()
+	toDoList := getFromDB(db)
+	InnerLogic(db, toDoList)
 	defer db.Close()
-	if err != nil{
-		log.Fatal("Error, Could Not OpenFile Line 108: ", err)
-	}
-	InnerLogic(db)
-
-
 }
